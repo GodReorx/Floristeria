46c576eaa4db73015ee62681d1aac438854c0e1e
Nuria-Fernandez <nuria.fernandez.torres15@gmail.com>
2024-03-06 13:19:30 +0100

    Factory con switch

--- src/main/java/FlowerStore/FlowerStoreFactory.java
+++ src/main/java/FlowerStore/FlowerStoreFactory.java
@@ -26,12 +26,12 @@ public class FlowerStoreFactory {
                 '}';
     }
 
-    public GardenElements getElement(String type, String characteristic, double price, int quantity) {
+    public GardenElements getElement(String type, String characteristic, double price) {
         GardenElements element = null;
         switch(type.toUpperCase()) {
-            case "FLOWER" -> element = new Flower(characteristic, price, quantity);
-            case "DECORATION" -> element = new Decoration(characteristic, price, quantity);
-            case "TREES" -> element = new Tree(characteristic, price, quantity);
+            case "FLOWER" -> element = new Flower(characteristic, price);
+            case "DECORATION" -> element = new Decoration(characteristic, price);
+            case "TREES" -> element = new Tree(characteristic, price);
             default -> throw new IllegalStateException("Unexpected value");
         }
         return element;
