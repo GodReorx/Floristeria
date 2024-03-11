9985f839bdad31660d47a9080f44a0029cd1aa03
GodReorx <eduard.granero@gmail.com>
2024-03-11 10:59:28 +0100

    Actualizacion conexion MySQL

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,9 +13,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("mysql:mysql-connector-java:8.0.33")
-    implementation("com.zaxxer:HikariCP:5.1.0")
-    implementation("org.slf4j:slf4j-api:2.0.12")
-    implementation("ch.qos.logback:logback-classic:1.5.3")
+    //implementation("com.zaxxer:HikariCP:5.1.0")
+    //implementation("org.slf4j:slf4j-api:2.0.12")
+    //implementation("ch.qos.logback:logback-classic:1.5.3")
 }
 
 tasks.test {
--- src/main/java/Connections/MySQL/GardenElementsMySQL.java
+++ src/main/java/Connections/MySQL/GardenElementsMySQL.java
@@ -5,8 +5,6 @@ import FlowerStore.Interfaces.GardenElements;
 import FlowerStoreFactory.Products.Decoration;
 import FlowerStoreFactory.Products.Flower;
 import FlowerStoreFactory.Products.Tree;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 
 import java.sql.*;
 import java.util.*;
@@ -14,21 +12,13 @@ import java.util.Date;
 
 public class GardenElementsMySQL<T extends GardenElements> implements GenericDAO {
 
-    private static final HikariDataSource dataSource;
+    private static final String  URL = "jdbc:mysql://" + Constants.MYSQL_SERVER + "/" + Constants.MYSQL_DATABASE;
     private static Connection connection;
-
-    static {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:mysql://" + Constants.MYSQL_SERVER + "/" + Constants.MYSQL_DATABASE);
-        config.setUsername(Constants.MYSQL_USERNAME);
-        config.setPassword(Constants.MYSQL_PASSWORD);
-        config.setMinimumIdle(10);
-        dataSource = new HikariDataSource(config);
-    }
 
     public GardenElementsMySQL() {
         try {
-            this.connection = dataSource.getConnection();
+            //this.connection = dataSource.getConnection();
+            connection = DriverManager.getConnection(URL,Constants.MYSQL_USERNAME,Constants.MYSQL_PASSWORD);
             System.out.println("Conectado a la bbdd");
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -39,7 +29,7 @@ public class GardenElementsMySQL<T extends GardenElements> implements GenericDAO
     private static void connectMySQL() {
         try {
             if (connection == null) {
-                connection = dataSource.getConnection();
+                connection = DriverManager.getConnection(URL,Constants.MYSQL_USERNAME,Constants.MYSQL_PASSWORD);
                 System.out.println("Conectado a la bbdd");
             }
         } catch (SQLException e) {
@@ -76,22 +66,18 @@ public class GardenElementsMySQL<T extends GardenElements> implements GenericDAO
         return flowerStores;
     }
     @Override
-    public int createStore(String name) {
+    public int createStore(String name) throws SQLException {
         int newStoreId = -1;
         connectMySQL();
         String query = "INSERT INTO FlowerShops (Name) VALUES (?)";
-        try
-            (PreparedStatement pstmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);){
-            pstmt.setString(1, name);
-            pstmt.executeUpdate();
-            ResultSet generatedKeys = pstmt.getGeneratedKeys();
-            if (generatedKeys.next()) {
-                newStoreId = generatedKeys.getInt(1);
-            } else {
-                throw new SQLException("Failed to get the generated ID for the new store.");
-            }
-        } catch (SQLException e) {
-            e.getMessage();
+        PreparedStatement pstmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        pstmt.setString(1, name);
+        pstmt.executeUpdate();
+        ResultSet generatedKeys = pstmt.getGeneratedKeys();
+        if (generatedKeys.next()) {
+            newStoreId = generatedKeys.getInt(1);
+        } else {
+            throw new SQLException("Failed to get the generated ID for the new store.");
         }
         //disconnectMySQL();
         return newStoreId;
@@ -99,48 +85,43 @@ public class GardenElementsMySQL<T extends GardenElements> implements GenericDAO
 
 
     @Override
-    public GardenElements findById(int id) {
+    public GardenElements findById(int id) throws SQLException {
         GardenElements gardenElement = null;
         connectMySQL();
         String query = "SELECT * FROM GardenElements WHERE IdGardenElements = ?";
-        try (PreparedStatement pstmt = connection.prepareStatement(query)) {
-            pstmt.setInt(1, id);
-            ResultSet rs = pstmt.executeQuery();
-            if (rs.next()) {
-                String type = rs.getString("TypeName"); // Supongamos que tienes un campo "type" en tu base de datos que indica el tipo de elemento (Flower, Tree, o Decoration)
-                switch (type.toUpperCase()) {
-                    case "FLOWER":
-                        gardenElement = new Flower(
-                                rs.getString("name"),
-                                rs.getInt("idProduct"),
-                                rs.getString("color"),
-                                rs.getDouble("price")
-                        );
-                        break;
-                    case "TREE":
-                        gardenElement = new Tree(
-                                rs.getString("name"),
-                                rs.getInt("idProduct"),
-                                rs.getString("size"),
-                                rs.getDouble("price")
-                        );
-                        break;
-                    case "DECORATION":
-                        gardenElement = new Decoration(
-                                rs.getString("name"),
-                                rs.getInt("idProduct"),
-                                rs.getString("typeMaterial"),
-                                rs.getDouble("price")
-                        );
-                        break;
-                    default:
-                        throw new IllegalArgumentException("Invalid type: " + type);
-                }
+        PreparedStatement pstmt = connection.prepareStatement(query);
+        pstmt.setInt(1, id);
+        ResultSet rs = pstmt.executeQuery();
+        if (rs.next()) {
+            String type = rs.getString("TypeName"); // Supongamos que tienes un campo "type" en tu base de datos que indica el tipo de elemento (Flower, Tree, o Decoration)
+            switch (type.toUpperCase()) {
+                case "FLOWER":
+                    gardenElement = new Flower(
+                            rs.getString("name"),
+                            rs.getInt("idProduct"),
+                            rs.getString("color"),
+                            rs.getDouble("price")
+                    );
+                    break;
+                case "TREE":
+                    gardenElement = new Tree(
+                            rs.getString("name"),
+                            rs.getInt("idProduct"),
+                            rs.getString("size"),
+                            rs.getDouble("price")
+                    );
+                    break;
+                case "DECORATION":
+                    gardenElement = new Decoration(
+                            rs.getString("name"),
+                            rs.getInt("idProduct"),
+                            rs.getString("typeMaterial"),
+                            rs.getDouble("price")
+                    );
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid type: " + type);
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            //disconnectMySQL();
         }
         return gardenElement;
     }
