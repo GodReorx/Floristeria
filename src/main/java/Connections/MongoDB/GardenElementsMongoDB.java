f7b8db1c1abc08ce4815cea265c4f613c1f2efed
GodReorx <eduard.granero@gmail.com>
2024-03-12 13:45:07 +0100

    Actualizacion conexion MongoDB

--- src/main/java/Connections/MongoDB/GardenElementsMongoDB.java
+++ src/main/java/Connections/MongoDB/GardenElementsMongoDB.java
@@ -7,8 +7,14 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoException;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.InsertOneResult;
+import org.bson.Document;
+import org.bson.types.ObjectId;
 
+import javax.print.Doc;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -64,6 +70,51 @@ public class GardenElementsMongoDB implements GenericDAO {
 
     @Override
     public int createStore(String name) {
+        MongoCollection<Document> collection = database.getCollection("FlowerShops");
+        Document newFlowerShop;
+        try {
+            List<Document> stock = new ArrayList<>();
+            stock.add(new Document("type", "tree")
+                    .append("Features", "3 m")
+                    .append("Quantity", 0)
+                    .append("Price", 0));
+            stock.add(new Document("type", "tree")
+                    .append("Features", "5 m")
+                    .append("Quantity", 0)
+                    .append("Price", 0));
+            stock.add(new Document("type", "tree")
+                    .append("Features", "50 cm")
+                    .append("Quantity", 0)
+                    .append("Price", 0));
+            stock.add(new Document("type", "flower")
+                    .append("Features", "Red")
+                    .append("Quantity", 0)
+                    .append("Price", 0));
+            stock.add(new Document("type", "flower")
+                    .append("Features", "Blue")
+                    .append("Quantity", 0)
+                    .append("Price", 0));
+            stock.add(new Document("type", "flower")
+                    .append("Features", "Green")
+                    .append("Quantity", 0)
+                    .append("Price", 0));
+            stock.add(new Document("type", "decoration")
+                    .append("Features", "Metal")
+                    .append("Quantity", 0)
+                    .append("Price", 0));
+            stock.add(new Document("type", "decoration")
+                    .append("Features", "Plastic")
+                    .append("Quantity", 0)
+                    .append("Price", 0));
+
+            newFlowerShop = new Document("_id", new ObjectId())
+                    .append("name", name)
+                    .append("stock", stock);
+            InsertOneResult result = collection.insertOne(newFlowerShop);
+        } catch (MongoException e) {
+            System.err.println("Error al conectar a la base de datos: " + e.getMessage());
+        }
+        //return newFlowerShop.get("_id");
         return 0;
     }
 
